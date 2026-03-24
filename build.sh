0b2189cb9cb41de37a433825018402a594cd6c8a
acollado-imas <acollado@i-mas.com>
2026-03-24 12:41:28 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BUILD_DIR="/home/acollado/Documentos/Proyectos/Trolley/QT_BUILDS/Desktop_Qt_6_10_1-Debug"
-QT_EXECUTABLE="/home/acollado/Qt/6.10.1/gcc_64/bin/"
-
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
-$QT_EXECUTABLE/qmake $ZED_WORKTREE_ROOT/SmartTrolley.pro -spec linux-g++ CONFIG+=qtquickcompiler
-bear -- make -j$(nproc)
-mv $BUILD_DIR/compile_commands.json $ZED_WORKTREE_ROOT
