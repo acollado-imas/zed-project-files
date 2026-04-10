4c0d408c5cacf6a92e31fc02ef08280d7dd024fc
acollado-imas <acollado@i-mas.com>
2026-04-10 11:03:53 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-BUILD_DIR="/home/acollado/Documentos/Proyectos/Trolley/QT_BUILDS/Desktop_Qt_6_10_1-Debug"
-QT_EXECUTABLE="/home/acollado/Qt/6.10.1/gcc_64/bin/"
+
+# Hay que definir la VENV "BUILD_DIR"
+# Hay que tener configurado en qtchooser un qt6
 
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
-$QT_EXECUTABLE/qmake $ZED_WORKTREE_ROOT/SmartTrolley.pro -spec linux-g++ CONFIG+=qtquickcompiler
-bear -- make -j$(nproc)
-mv $BUILD_DIR/compile_commands.json $ZED_WORKTREE_ROOT
+QT_SELECT=qt6 qmake $ZED_WORKTREE_ROOT -spec linux-g++ CONFIG+=qtquickcompiler
+make -j$(nproc)
